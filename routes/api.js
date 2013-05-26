206e57c832ff5143f82003bb033e66e653fe5292
Jean Lauliac <lauliacj@gmail.com>
2013-05-25 20:17:07 -0400

    Use the full beer list.

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,5 @@
 var _ = require('lodash');
-var beerList = _.unique(require('../mockdata/beers.json'), function(beer) {
-    return beer.beerId;
-});
+var beerList = require('../mockdata/unique_beers.json');
 
 exports.beers = function(request, response){
     var filter = new RegExp(request.param('filter'), 'i');
@@ -12,5 +10,5 @@ exports.beers = function(request, response){
     results = _.sortBy(results, function(beer) {
         return !orderFilter.test(beer.name);
     });
-    response.send(results);
+    response.json(results);
 };
